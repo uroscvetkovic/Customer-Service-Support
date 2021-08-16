37fcadb30fa207af92ada54a52504b41058cb050
framus-g <uroscvetkovic5@gmail.com>
2021-08-16 15:20:48 +0200

    admin display list, set sorting by date/time callback and submited time, filter by archived

--- backend/customer_service_supports/admin.py
+++ backend/customer_service_supports/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import CustomerServiceSupport
 
-admin.site.register(CustomerServiceSupport)
+@admin.register(CustomerServiceSupport)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'date_time_callback', 'submited_at', 'archived' )
+    sortable_by = ('date_time_callback', 'submited_at')
+    list_filter = ['archived']
--- backend/customer_service_supports/models.py
+++ backend/customer_service_supports/models.py
@@ -11,7 +11,7 @@ class CustomerServiceSupport(models.Model):
     date_time_callback = models.DateTimeField(blank=True, null=True)
     submited_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(blank=True)
-    arhived = models.BooleanField(default=False)
+    archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.subject
